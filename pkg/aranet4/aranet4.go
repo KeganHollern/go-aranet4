3468fda578aaeeb30dda781637f27d84bb8aca47
Kegan Hollern <keganhollern@gmail.com>
2022-09-20 19:09:08 -0500

    initial aranet4 connect and disconnect

--- pkg/aranet4/aranet4.go
+++ pkg/aranet4/aranet4.go
@@ -1,26 +1,52 @@
 package aranet4
 
 import (
-	"errors"
 	"github.com/KeganHollern/go-aranet4/pkg/internal"
 )
 
 // A4Device defines an interface for interacting with Aranet4 Bluetooth Devices.
 type A4Device interface {
 	Connect() error
+	Disconnect()
 }
 
 // NewDeviceFromMAC returns an A4Device from the provided bluetooth MAC address.
 // If the provided MAC is not found (or paired), an error is returned.
+//
+//	device, err := aranet4.NewDeviceFromMAC("00:00:00:00:00:00")
+//	/* handle err */
+//	err = device.Connect()
+//	/* handle err */
+//	defer device.Disconnect()
+//	...
 func NewDeviceFromMAC(MAC string) (A4Device, error) {
-
-	device := &internal.Aranet4Device{}
-
-	return nil, errors.New("not implemented")
+	a4 := &internal.Aranet4Device{
+		Mac: MAC,
+	}
+	err := a4.ScanForDevice()
+	if err != nil {
+		return nil, err
+	}
+	return a4, nil
 }
 
 // GetNearbyDevices returns a list of A4Device for each nearby Aranet4 bluetooth device.
 // Note that only paired Aranet4 devices are returned.
+// Devices which fail to connect are dropped without error.
 func GetNearbyDevices() ([]A4Device, error) {
-	return nil, errors.New("not implemented")
+	var results []A4Device
+	macs, err := internal.FindDevices()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, mac := range macs {
+		device, err := NewDeviceFromMAC(mac)
+		if err != nil {
+			continue
+		}
+		results = append(results, device)
+	}
+
+	return results, nil
 }
--- pkg/internal/aranet4.go
+++ pkg/internal/aranet4.go
@@ -1,6 +1,11 @@
 package internal
 
-import "tinygo.org/x/bluetooth"
+import (
+	"errors"
+	"strings"
+
+	"tinygo.org/x/bluetooth"
+)
 
 var (
 	globalAdapter = bluetooth.DefaultAdapter
@@ -8,23 +13,66 @@ var (
 
 type Aranet4Device struct {
 	Mac string
+
+	scan   bluetooth.Addresser
+	device *bluetooth.Device
 }
 
-func (dev Aranet4Device) Connect() error {
+func FindDevices() ([]string, error) {
+	var results []string
 	globalAdapter.Enable()
-	var foundDevice bluetooth.ScanResult
+	err := globalAdapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
+		if strings.Contains(strings.ToLower(result.LocalName()), "aranet4") {
+			results = append(results, result.Address.String())
+		}
+	})
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func (dev Aranet4Device) ScanForDevice() error {
+	globalAdapter.Enable()
+
 	err := globalAdapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
 		if result.Address.String() == dev.Mac {
-			foundDevice = result
+			dev.scan = result.Address
 			err := adapter.StopScan()
 			if err != nil {
-				panic(err) // unrecoverable
+				panic(err)
 			}
 		}
 	})
 	if err != nil {
 		return err
 	}
+	if dev.scan == nil {
+		return errors.New("device not found")
+	}
+	return nil
+}
 
-	device, err := globalAdapter.Connect()
+func (dev Aranet4Device) Connect() error {
+	if dev.scan == nil {
+		err := dev.ScanForDevice() // scan again ?
+		if err != nil {
+			return err
+		}
+	}
+
+	var err error
+	dev.device, err = globalAdapter.Connect(dev.scan, bluetooth.ConnectionParams{})
+	if err != nil {
+		return err
+	}
+	if dev.device == nil {
+		return errors.New("failed to connect")
+	}
+	return nil
+}
+func (dev Aranet4Device) Disconnect() {
+	if dev.device != nil {
+		dev.device.Disconnect()
+	}
 }
